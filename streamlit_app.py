a30f1a66ef2f979df15c25007984924ff94a2b9f
Krasito18 <chrisbj1337@gmail.com>
2026-08-29 13:10:14 +0300

    changing up template

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.write(
 )
 
 
-# Let the user upload a file via `st.file_uploader`.
+# Let the user upload a file via `st.file_uploader`
 uploaded_file = st.file_uploader(
     "Upload a document (.txt or .md)", type=("txt", "md")
 )
